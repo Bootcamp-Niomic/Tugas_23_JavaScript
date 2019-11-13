078d3973a1f24c1bf245d6cb1f635083c467d4f4
Havid <great_havid@yahoo.co.id>
2019-11-13 11:46:15 +0700

    Tugas 23 JS

---
+++ tugas23.js
@@ -0,0 +1,8 @@
+// ====== Tugas 23 JavaScript =====
+
+function init(value) {
+    return value > 15;
+  }
+  
+  var data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66].filter(init)
+  console.log(data)
